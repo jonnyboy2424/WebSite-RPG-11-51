c62e1d9a65cc68676d7d882a7a25ddb0b10abade
jonnyboy2424 <jonathanlouisowens@gmail.com>
2025-01-09 11:52:25 -0600

    Help
    I do

---
+++ bird-route.js
@@ -0,0 +1,132 @@
+
+import { birdStoryText } from "./storylineText.js";
+import {birdStats} from "./character-stats.js"
+import {takeDamage, updateStoryline, cigaretteDecrement} from "./front-end.js"
+export function birdRoute() {
+  let cigarettesRemaining = 12;
+  let powerRanking = 0
+  let storyline = document.getElementById("story-narration")
+  const peckAction = document.getElementById("Peck")
+  const cigaretteOffer = document.getElementById('Cigarette Offer')
+  const peck2x = document.getElementById("Peck 2x")
+  const cigaretteOffer2x = document.getElementById("Cigarette Offer 2x")
+  peck2x.classList.add("unlearnedMoves")
+  cigaretteOffer2x.classList.add("unlearnedMoves")
+  const cigarettesLeft = document.getElementById('cigaretteTotal')
+  let currentStoryIndex = 0;
+  let health = birdStats.healthLeft
+  
+
+
+
+  cigarettesLeft.innerHTML = `<p> Cigarettes Remaining: ${cigarettesRemaining} </p>`
+  
+  storyline.innerText = birdStoryText.startingLine
+   
+    peckAction.addEventListener("click", function(){
+      switch(currentStoryIndex) {
+        case 0:
+          updateStoryline(birdStoryText.peckTheFemale);
+          takeDamage(1)
+          break;
+        case 1:
+          updateStoryline(birdStoryText.peckBuckyAdams);
+          break;
+        case 2:
+          updateStoryline(birdStoryText.peckFamily);
+          break;
+        case 3:
+          updateStoryline(birdStoryText.peckFBI);
+          break;
+        case 4:
+          updateStoryline(birdStoryText.peckDarcy);
+          break;
+        case 5:
+          updateStoryline(birdStoryText.peckCat);
+          break;
+        case 6:
+          updateStoryline(birdStoryText.peckMrTabacco);
+          break;
+        case 7:
+          updateStoryline(birdStoryText.peckDoomsdayPrepper);
+          break;
+        case 8:
+          updateStoryline(birdStoryText.peckMrHeart);
+          break;}
+
+
+})
+
+  cigaretteOffer.addEventListener("click", function(){
+    switch(currentStoryIndex) {
+          case 0:
+            updateStoryline(birdStoryText.cigTheFemale);
+            takeDamage(1)
+            break;
+          case 1:
+            updateStoryline(birdStoryText.cigBuckyAdams);
+            break;
+          case 2:
+            updateStoryline(birdStoryText.cigFamily);
+            break;
+          case 3:
+            updateStoryline(birdStoryText.cigFBIAgent);
+            break;
+          case 4:
+            updateStoryline(birdStoryText.cigDarcy);
+            break;
+          case 5:
+            updateStoryline(birdStoryText.cigCat);
+            break;
+          case 6:
+            updateStoryline(birdStoryText.cigTabacco);
+            break;
+          case 7:
+            updateStoryline(birdStoryText.cigZombiePrepper);
+            break;
+          case 8:
+            updateStoryline(birdStoryText.cigMrHeart);
+            break;
+  
+  
+        }
+      cigaretteDecrement(1);
+      currentStoryIndex++
+      console.log(currentStoryIndex)
+    })
+  
+    cigaretteOffer2x.addEventListener("click", function(){
+      switch(currentStoryIndex) {
+        case 2:
+          updateStoryline(birdStoryText.cigFamilyTwice);
+          break;
+        case 3:
+          updateStoryline(birdStoryText.cigFBIAgentTwice);
+          break;
+        case 4:
+          updateStoryline(birdStoryText.cigDarcyTwice);
+          break;
+        case 5:
+          updateStoryline(birdStoryText.cigCatTwice);
+          break;
+        case 6:
+          updateStoryline(birdStoryText.cigTabaccoTwice);
+          break;
+        case 7:
+          updateStoryline(birdStoryText.cigZombiePrepperTwice);
+          break;
+        case 8:
+          updateStoryline(birdStoryText.cigMrHeartTwice);
+          break;
+}
+  cigaretteDecrement(2);
+  currentStoryIndex++;
+})
+  
+  
+  
+  
+  
+  
+  }
+
---
+++ character-stats.js
@@ -0,0 +1,34 @@
+export let woodenBoyStats = {
+  healthLeft: 5,
+  location: "Toy Factory",
+  backgroundColor: "Grey",
+  playerCharacter: "Wooden Boy",
+  skills:['Stab', 'Stab II', 'Stab III', 'Negotiate', 'Please No Fighting!', 'First-Aid', 'First-Aid', 'First-Aid'],
+  headerIcon: `<img class="character-select-img" src="images/woodenBoy.jpg">`}
+  // the ending is determined by how many people the wooden Boy kills.
+
+export let pepperStats = {
+  healthLeft: 3,
+  location: "Hell",
+  backgroundColor: "darkred",
+  playerCharacter: "Pepper",
+  skills:['Gnashing of Teeth', 'Run Away!', 'Solar Flare', 'Solar Flare'],
+  headerIcon: `<img class="character-select-img" src="images/black_pepper.jpg">`}
+  // On the pepper route, his path is more luck based. Can win pocket knives that will help him with the final boss. Needs 2 Pocket Knives to get good ending.
+
+export let furballStats = {
+  healthLeft: 8,
+  location: "Mythical Forest",
+  backgroundColor: "Green",
+  playerCharacter: "Furball",
+  skills: ['Kick', 'Ice Breath', 'Ice Breath', 'Ice Breath', 'Gorge', 'Gorge', 'Solar Flare'] ,
+  headerIcon: `<img class="character-select-img" src="images/furball.webp">`}
+  // Traditional JRPG route. Learns moves. Gets best ending if he uses Galaxia Blast on the Mountain Dragon
+
+export let birdStats = {
+  healthLeft: 6,
+  location: "Gas Station",
+  backgroundColor: "Pink",
+  playerCharacter: "Furball",
+  skills: ['Peck', 'Cigarette Offer','Cigarette Offer 2x', 'Peck 2x'] ,
+  headerIcon: `<img class="character-select-img" src="images/bird.webp">`}
