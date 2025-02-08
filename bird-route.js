43b5d372f36a6c2131a63286049c247f8e120f7b
jonnyboy2424 <jonathanlouisowens@gmail.com>
2025-02-07 22:39:33 -0600

    bad change
    bad change

---
+++ bird-route.js
@@ -0,0 +1,269 @@
+
+import { birdStoryText } from "./storylineText.js";
+import {birdStats} from "./character-stats.js"
+import {updateStoryline, healthBar} from "./front-end.js"
+export function birdRoute() {
+  let cigarettesRemaining = 12;
+  let storyline = document.getElementById("story-narration")
+  const peckAction = document.getElementById("Peck")
+  const cigaretteOffer = document.getElementById('Cigarette Offer')
+  const peck2x = document.getElementById("Peck 2x")
+  const cigaretteOffer2x = document.getElementById("Cigarette Offer 2x")
+  const continueGame = document.getElementById("Continue")
+  continueGame.classList.add("unlearnedMoves")
+  peck2x.classList.add("unlearnedMoves")
+  cigaretteOffer2x.classList.add("unlearnedMoves")
+  const cigarettesLeft = document.getElementById('cigaretteTotal')
+  let currentStoryIndex = 0;
+  let health = birdStats.healthLeft
+  let totalFriendshipPoints = 0; 
+   
+  
+  //bird exclusive game mechanic 
+  function cigaretteDecrement (param) {
+    if (cigarettesRemaining === 0) {
+      cigarettesLeft.innerText = `Cigarettes Remaining: None`
+      document.getElementById(``)
+    } else {
+      cigarettesRemaining -= param;
+      cigarettesLeft.innerText = `Cigarettes Remaining: ${cigarettesRemaining}`
+    }
+    console.log(`You gave a cigarette away. Now you have ${cigarettesRemaining}`)
+  }
+
+  function takeAllCigarettes() {
+    cigarettesRemaining -= cigarettesRemaining;
+    cigarettesLeft.innerText = `Cigarettes Remaining: None`
+    document.getElementById(``);
+    if (cigaretteOffer2x) {cigaretteOffer2x.classList.add("unlearnedMoves");}
+    cigaretteOffer.classList.add('unlearnedMoves')
+  }
+
+  function cigaretteGift(){
+    cigarettesRemaining++;
+    cigarettesLeft.innerText = `Cigarettes Remaining: ${cigarettesRemaining}`
+  }
+
+//take damage game mechanic
+function takeDamage (damage) {
+  health -= damage;
+  healthBar.innerHTML = `<p> Health Left: ${health} </p>`
+}
+
+// heal game mechanic
+function heal(){
+  health++;
+  healthBar.innerHTML = `<p> Health Left: ${health} </p>`;
+}
+
+// this function determines the ending
+function endingDetermine(friendshipPoints){
+  const requiredFriendshipPoints = 4; 
+  
+  if (friendshipPoints < requiredFriendshipPoints && cigarettesRemaining > 0){
+    cigaretteDecrement(1)
+    updateStoryline(birdStoryText.badEnding);
+  }
+  else if (friendshipPoints < requiredFriendshipPoints && cigarettesRemaining === 0){
+    updateStoryline(birdStoryText.badEndingWithNoCig)
+  }
+  else if (friendshipPoints >= requiredFriendshipPoints && birdGotCigarette){
+    updateStoryline(birdStoryText.goodEndingWithFemaleBird);
+    currentStoryIndex++;
+    continueGame.classList.add("unlearnedMoves");
+    peckAction.classList.remove("unlearnedMoves");
+    cigaretteOffer.classList.remove("unlearnedMoves");
+    peck2x.classList.remove("unlearnedMoves");
+    cigaretteOffer2x.classList.remove("unlearnedMoves");
+  }
+}
+
+
+
+// continue function that removes all moves and leaves only 'continue'
+function choiceJudgement(){
+  continueGame.classList.remove("unlearnedMoves");
+  peckAction.classList.add("unlearnedMoves");
+  cigaretteOffer.classList.add("unlearnedMoves");
+  peck2x.classList.add("unlearnedMoves");
+  cigaretteOffer2x.classList.add("unlearnedMoves");
+}
+
+  cigarettesLeft.innerHTML = `<p> Cigarettes Remaining: ${cigarettesRemaining} </p>`
+  
+  storyline.innerText = birdStoryText.startingLine
+
+    continueGame.addEventListener("click", function(){
+      switch(currentStoryIndex){
+        case 10:
+          endingDetermine(totalFriendshipPoints)
+          break;
+      }
+    })
+   
+    peckAction.addEventListener("click", function(){
+      switch(currentStoryIndex) {
+        case 0:
+          updateStoryline(birdStoryText.peckTheFemale);
+          takeDamage(1);
+          
+          break;
+        case 1:
+          updateStoryline(birdStoryText.peckBuckyAdams);
+          break;
+        case 2:
+          updateStoryline(birdStoryText.peckFamily);
+          break;
+        case 3:
+          updateStoryline(birdStoryText.peckFBI);
+          peck2x.classList.remove("unlearnedMoves");
+          break;
+        case 4:
+          updateStoryline(birdStoryText.peckDarcy);
+          takeDamage(1)
+          break;
+        case 5:
+          updateStoryline(birdStoryText.peckCat);
+          takeDamage(2);
+          break;
+        case 6:
+          updateStoryline(birdStoryText.peckMrTabacco);
+          takeAllCigarettes();
+          break;
+        case 7:
+          updateStoryline(birdStoryText.peckDoomsdayPrepper);
+          break;
+        case 8:
+          updateStoryline(birdStoryText.peckMrHeart);
+          break;
+        case 9:
+          updateStoryline(birdStoryText.peckGasStationClerk);
+          choiceJudgement();
+          break;
+        
+        }
+
+          currentStoryIndex++
+
+})
+
+  cigaretteOffer.addEventListener("click", function(){
+    switch(currentStoryIndex) {
+          case 0:
+            updateStoryline(birdStoryText.cigTheFemale);
+            cigaretteDecrement(1);
+            let birdGotCigarette = true;
+            break;
+          case 1:
+            updateStoryline(birdStoryText.cigBuckyAdams);
+            cigaretteOffer2x.classList.remove("unlearnedMoves");
+            break;
+          case 2:
+            updateStoryline(birdStoryText.cigFamily);
+            takeDamage(1);
+            break;
+          case 3:
+            updateStoryline(birdStoryText.cigFBIAgent);
+            break;
+          case 4:
+            updateStoryline(birdStoryText.cigDarcy);
+            break;
+          case 5:
+            updateStoryline(birdStoryText.cigCat);
+            takeDamage(2);
+            break;
+          case 6:
+            updateStoryline(birdStoryText.cigTabacco);
+            heal()
+            break;
+          case 7:
+            updateStoryline(birdStoryText.cigZombiePrepper);
+            takeDamage(1);
+            break;
+          case 8:
+            updateStoryline(birdStoryText.cigMrHeart);
+            break;
+          case 9:
+            updateStoryline(birdStoryText.cigGasStationClerk);
+            choiceJudgement();
+            break;
+  
+  
+        }
+      cigaretteDecrement(1);
+      currentStoryIndex++
+      console.log(currentStoryIndex)
+    })
+  
+    cigaretteOffer2x.addEventListener("click", function(){
+      switch(currentStoryIndex) {
+        case 2:
+          updateStoryline(birdStoryText.cigFamilyTwice);
+          takeDamage(1);
+          break;
+        case 3:
+          updateStoryline(birdStoryText.cigFBIAgentTwice);
+          break;
+        case 4:
+          updateStoryline(birdStoryText.cigDarcyTwice);
+          break;
+        case 5:
+          updateStoryline(birdStoryText.cigCatTwice);
+          takeDamage(2);
+          break;
+        case 6:
+          updateStoryline(birdStoryText.cigTabaccoTwice);
+          break;
+        case 7:
+          updateStoryline(birdStoryText.cigZombiePrepperTwice);
+          takeDamage(1);
+          break;
+        case 8:
+          updateStoryline(birdStoryText.cigMrHeartTwice);
+          takeDamage(1);
+          break;
+        case 9:
+          updateStoryline(birdStoryText.cigGasStationClerkTwice);
+          choiceJudgement();
+          break;
+}
+  cigaretteDecrement(2);
+  currentStoryIndex++;
+})
+
+peck2x.addEventListener("click", function(){
+  switch(currentStoryIndex) {
+    case 4:
+      updateStoryline(birdStoryText.peckDarcyTwice);
+      break;
+    case 5:
+      updateStoryline(birdStoryText.peckCatTwice);
+      cigaretteGift()
+      break;
+    case 6:
+      updateStoryline(birdStoryText.peckTabaccoTwice);
+      break;
+    case 7:
+      updateStoryline(birdStoryText. peckDoomsdayPrepperTwice);
+      break;
+    case 8:
+      updateStoryline(birdStoryText.peckMrHeartTwice);
+      takeDamage(1);
+      break;
+    case 9:
+      updateStoryline(birdStoryText.peckGasStationClerkTwice);
+      choiceJudgement();
+      break; 
+};
+
+
+currentStoryIndex++;
+})
+  
+  
+  
+  
+
+  
+  }
+
--- front-end.js
+++ front-end.js
@@ -11,20 +11,19 @@ const gameOverContainer = document.getElementById("gameOverScreen")
 const miscContainer = document.getElementById("misc-container")
 
 let skills = [];
-import { pepperRoute } from './pepper-route.js'
-import { birdRoute } from './bird-route.js'
-import { woodenBoyStats, pepperStats, furballStats, birdStats } from "./character-stats.js"
+import {birdRoute} from './bird-route.js'
+import {woodenBoyStats, pepperStats, furballStats, birdStats} from "./character-stats.js"
 
 
 //woodenBoy setup pacifist run or genocide run, pacifist run is hard to achieve and will require multiple runs //
-woodenBoy.addEventListener("mouseover", function () {
+woodenBoy.addEventListener ("mouseover", function(){
   document.getElementById("startingDesc").innerHTML = "A small wooden soldier who fights with it's trusty toy sword. Wants a better one though. Recently just learned that it is alive. Doesn't know how to behave so that is up to you to decide."
-
+  
 
 })
 
 
-woodenBoy.addEventListener("dblclick", function () {
+woodenBoy.addEventListener ("dblclick", function(){
   console.log('You have chosen to play as the wooden boy.')
   beginRoute(woodenBoyStats)
 })
@@ -34,15 +33,14 @@ woodenBoy.addEventListener("dblclick", function () {
 
 //pepper setup, pepper can pick up pocket knives and first aid kits on his journey by RNG, very hard to complete// 
 
-pepper.addEventListener("mouseover", function () {
+pepper.addEventListener ("mouseover", function(){
   document.getElementById("startingDesc").innerHTML = "A flying black thing. In no way, does this 'thing' have any offensive prowess unless it happens to find a pocket-knife on its journey. Has had enough of hell so he is trying to get out. This journey is luck-based so every attempt will be different and beyond your control. Isn't that comforting?"
-
+  
 })
 
-pepper.addEventListener("dblclick", function () {
+pepper.addEventListener ("dblclick", function(){
   beginRoute(pepperStats)
-  pepperRoute();
-
+    
 
 
 
@@ -50,26 +48,26 @@ pepper.addEventListener("dblclick", function () {
 
 //furball setup, a riddle adventure//
 
-furBall.addEventListener("mouseover", function () {
+furBall.addEventListener ("mouseover", function(){
   document.getElementById("startingDesc").innerHTML = "A curious ball of fur with bright red shoes. I don't know much about his (her?) character motivations. Very silent and unexpressive. This fur ball does not have much going on. Enjoy the romp."
 
 
 })
 
-furBall.addEventListener("dblclick", function () {
+furBall.addEventListener ("dblclick", function(){
   beginRoute(furballStats);
 })
 
 
 //bird setup, a zombie apocalypse is occuring at the gas station and bird's success is dependant on who he has been nice to in the prelude // 
 
-bird.addEventListener("mouseover", function () {
+bird.addEventListener ("mouseover", function(){
   document.getElementById("startingDesc").innerHTML = "A tall stocky bird who can reach great heights. Very strong but has a chronic addiction to chain-smoking cigarettes. In the gas station that he lives in, there is an incoming raid of zombies that only he knows about. Make friends wisely. Just because you can bum them a cigarette, does not mean they necessarily have your back."
-
+  
 
 })
 
-bird.addEventListener("dblclick", function () {
+bird.addEventListener ("dblclick", function(){
   beginRoute(birdStats);
   birdRoute()
 })
@@ -79,8 +77,8 @@ bird.addEventListener("dblclick", function () {
 
 
 
-export function moveDisplay(param) {
-  for (let i = 0; i < param.length; i++) {
+export function moveDisplay(param){
+  for ( let i = 0; i < param.length; i++) {
     moveContainer.innerHTML += `<button id="${param[i]}"> ${param[i]}</button>`
   }
 
@@ -91,19 +89,19 @@ export function moveDisplay(param) {
 
 
 
-function beginRoute(param) {
+function beginRoute (param){
 
-
-  aboutSection.style.display = 'none'
-  miscContainer.style.display = 'none'
-  characterSelectScreen.style.display = "none";
-  birdsAdventure.style.display = 'block';
-  console.log("working")
-  healthBar.innerHTML = `<p> Health Left: ${param.healthLeft} </p>`
-  gameHeader.innerHTML = param.headerIcon;
-  moveDisplay(param.skills);
-  gameOverContainer.style.visibility = "hidden"
-  birdsAdventure.style.backgroundColor = param.backgroundColor;
+  
+    aboutSection.style.display= 'none'
+    miscContainer.style.display= 'none'
+    characterSelectScreen.style.display="none";
+    birdsAdventure.style.display= 'block';
+    console.log("working")
+    healthBar.innerHTML = `<p> Health Left: ${param.healthLeft} </p>`
+    gameHeader.innerHTML = param.headerIcon;
+    moveDisplay(param.skills);
+    gameOverContainer.style.visibility="hidden"
+    birdsAdventure.style.backgroundColor = param.backgroundColor;
 
 }
 
@@ -111,10 +109,10 @@ function beginRoute(param) {
 
 
 
-export function updateStoryline(param) {
+export function updateStoryline (param) {
   let storyline = document.getElementById("story-narration")
   storyline.innerText = param
-
+  
 }
 
 
@@ -135,11 +133,10 @@ closeButton.addEventListener("click", aboutSectionDisplay)
 let isOpen = false
 
 function aboutSectionDisplay() {
-  if (isOpen) {
-    aboutSection.style.display = 'none'
-  }
+  if (isOpen){
+  aboutSection.style.display= 'none'}
   else {
-    aboutSection.style.display = "block"
+    aboutSection.style.display="block"
   }
   isOpen = !isOpen;
 }
