d0fe228ba93c742ae9c033c812a6a815a050927d
jonnyboy2424 <jonathanlouisowens@gmail.com>
2025-02-07 22:34:34 -0600

    bad

---
+++ Scripts/pepper_storyline.js
@@ -0,0 +1,4 @@
+export const pepperStoryText = {
+  //peck options
+  startingLine: `You have spent 11,903, 243, 243 years in the underworld.It is hot but you’ve gotten used to it by now.But  now, you have grown tired of staring at the stalactite and stalagmites that adorn your cavern.You flutter out of your little hovel.In front of you is your neighbor, Sally Mander.Once again, he is slithering around, rubbing his belly all against your flowers. You wake up in your usual dumpster. There is another bird next to you. Her eyes are closed. What do you do?"`
+}
